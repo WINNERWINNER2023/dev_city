cb9301f36880c8e7c44fb3dc2a5783712748f30c
cchoseonghun <109499274+cchoseonghun@users.noreply.github.com>
2023-02-07 17:47:44 +0900

    test: UploadUtil, AdminAuthMiddleware 유닛 테스트 임시 저장

---
+++ __tests__/unit/middlewares/AdminsAuthMiddleware.unit.spec.js
@@ -0,0 +1,11 @@
+const adminsAuthMiddleware = require('../../../src/middlewares/adminsAuthMiddleware');
+
+describe('adminsAuthMiddleware Unit Test', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  test('adminsAuthMiddleware Method Success', async () => {
+
+  });
+});
